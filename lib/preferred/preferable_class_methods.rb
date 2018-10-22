310fedb1e1b2e50e77c631b7a15c8edb76202459
Joe D'Arcangelo <joedarc@gmail.com>
2018-10-22 15:18:05 -0400

    Allow should_prefix option

--- lib/preferred/preferable_class_methods.rb
+++ lib/preferred/preferable_class_methods.rb
@@ -2,21 +2,22 @@ module Preferred::PreferableClassMethods
 
   def preference(name, type, *args)
     options = args.extract_options!
-    options.assert_valid_keys(:default, :description)
+    options.assert_valid_keys(:default, :description, :should_prefix)
     default = options[:default]
     description = options[:description] || name
+    should_prefix = options[:should_prefix] || true
 
     # cache_key will be nil for new objects, then if we check if there
     # is a pending preference before going to default
-    define_method preference_getter_method(name) do
+    define_method preference_getter_method(name, should_prefix) do
       if self.preference_hash && preference_hash.key?(name.to_s)
         convert_preference_value(preference_hash[name.to_s]["value"], preference_hash[name.to_s]["type"])
       else
-        send self.class.preference_default_getter_method(name)
+        send self.class.preference_default_getter_method(name, should_prefix)
       end
     end
 
-    define_method preference_setter_method(name) do |value|
+    define_method preference_setter_method(name, should_prefix) do |value|
       value = convert_preference_value(value, type)
       self.preference_hash = {} if preference_hash.nil?
       self.preference_hash[name.to_s] = {
@@ -25,15 +26,15 @@ module Preferred::PreferableClassMethods
       }
     end
 
-    define_method preference_default_getter_method(name) do
+    define_method preference_default_getter_method(name, should_prefix) do
       default
     end
 
-    define_method preference_type_getter_method(name) do
+    define_method preference_type_getter_method(name, should_prefix) do
       type
     end
 
-    define_method preference_description_getter_method(name) do
+    define_method preference_description_getter_method(name, should_prefix) do
       description
     end
   end
@@ -46,24 +47,28 @@ module Preferred::PreferableClassMethods
     remove_method preference_description_getter_method(name) if method_defined? preference_description_getter_method(name)
   end
 
-  def preference_getter_method(name)
-    "preferred_#{name}".to_sym
+  def preference_getter_method(name, should_prefix)
+    "#{preferred_prefix(should_prefix)}#{name}".to_sym
   end
 
-  def preference_setter_method(name)
-     "preferred_#{name}=".to_sym
+  def preference_setter_method(name, should_prefix)
+     "#{preferred_prefix(should_prefix)}#{name}=".to_sym
   end
 
-  def preference_default_getter_method(name)
-    "preferred_#{name}_default".to_sym
+  def preference_default_getter_method(name, should_prefix)
+    "#{preferred_prefix(should_prefix)}#{name}_default".to_sym
   end
 
-  def preference_type_getter_method(name)
-    "preferred_#{name}_type".to_sym
+  def preference_type_getter_method(name, should_prefix)
+    "#{preferred_prefix(should_prefix)}#{name}_type".to_sym
   end
 
-  def preference_description_getter_method(name)
-    "preferred_#{name}_description".to_sym
+  def preference_description_getter_method(name, should_prefix)
+    "#{preferred_prefix(should_prefix)}#{name}_description".to_sym
+  end
+
+  def preferred_prefix(should_prefix)
+    should_prefix ? "preferred_" : nil
   end
 
 end
